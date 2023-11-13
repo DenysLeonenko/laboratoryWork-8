0c218df14e19398ae23859a57d0c2790b4b086f1
DenysLeonenko <leonenkodendev@gmail.com>
2023-11-13 14:08:34 +0200

    LaboratoryWork-8

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
+input_word = input("Введіть ключове слово: ")
 text_file = open("text.txt", encoding="utf-8")
 text = text_file.read()
 
 if text.__len__() == 0:
     print("Файл порожній!")
 else:
-    signs = ['.', ',', ';', ':', '!', '?']
     paragraphs = text.split("\n\n")
 
     print("Абзаци зі словами-тегами:" + "\n")
     for paragraph in paragraphs:
         words_in_paragraph = paragraph.split()
         for word in words_in_paragraph:
-            if word.startswith('#') and not any(word.endswith(sign) for sign in signs):
+            if word.startswith('#') and input_word.lower() in word.lower():
                 print(paragraph + "\n")
                 break
